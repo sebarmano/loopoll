6480a58651628ba3c4e465ea584b3f6c43f6ac3b
Sebastian Armano <sebarmano@gmail.com>
2014-07-04 10:46:18 -0400

    commented icheck lines in application.js to avoid invald token issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,17 +21,17 @@
 
 var picker = new Pikaday({ field: $( '#quickq_question_duedate')[0]});
 
-$(document).ready(function(){
-  $('input').each(function(){
-    var self = $(this),
-      label = self.next(),
-      label_text = label.text();
-
-    label.remove();
-    self.iCheck({
-      checkboxClass: 'icheckbox_line',
-      radioClass: 'iradio_line',
-      insert: '<div class="icheck_line-icon"></div>' + label_text
-    });
-  });
-});
+// $(document).ready(function(){
+//   $('input').each(function(){
+//     var self = $(this),
+//       label = self.next(),
+//       label_text = label.text();
+//
+//     label.remove();
+//     self.iCheck({
+//       checkboxClass: 'icheckbox_line',
+//       radioClass: 'iradio_line',
+//       insert: '<div class="icheck_line-icon"></div>' + label_text
+//     });
+//   });
+// });
