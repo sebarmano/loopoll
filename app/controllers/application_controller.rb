a61f5dc3ec50a5583846c3a23ba482c1a507a18d
Sebastian Armano <sebarmano@gmail.com>
2014-07-16 08:00:59 -0400

    removed email from users data in api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery 
   before_action :require_login
 
   helper_method :current_user
@@ -17,7 +17,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-
-
 end
--- app/views/api/v1/questions/index.json.jbuilder
+++ app/views/api/v1/questions/index.json.jbuilder
@@ -3,7 +3,6 @@ json.questions @questions do |question|
 	json.creator do
 		json.id question.user.id
 		json.name question.user.name
-		json.email question.user.email
 		json.questions question.user.questions.count
 		json.answers	question.user.results.count
 	end
